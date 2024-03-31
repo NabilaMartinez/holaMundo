5bf29ea2d4b5883d8d42cf779817a09f6f245241
Nabyla <nabyla18@gmail.com>
2024-03-31 19:41:36 +0200

    Commit inicial de hola mundo

---
+++ src/Primitivos.java
@@ -0,0 +1,11 @@
+public class Primitivos
+{
+    public static void main(String[] args)
+    {
+     byte numeroByte = 127;
+       System.out.println("numeroByte = " + numeroByte);
+        System.out.println("tipo byte corresponde en byte a " +Byte.BYTES);
+        System.out.println("tipo byte corresponde en bites a " +Byte.SIZE);
+
+    }
+}
